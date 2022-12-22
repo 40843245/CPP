d8aad234f33ee2469c06094761260c30d0514795
40843245 <75050655+40843245@users.noreply.github.com>
2022-12-22 20:51:58 +0800

    Create raw_storage_iterator_ex1.cpp

---
+++ Library/memory/iterator/raw_storage_iterator_ex1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main () 
+{
+  std::vector<std::string> myvector;
+  myvector.push_back ("first");
+  myvector.push_back ("second");
+  myvector.push_back ("third");
+
+  std::pair<std::string*,std::ptrdiff_t> result = std::get_temporary_buffer<std::string>(3);
+  std::string* pstr=result.first;
+
+  std::raw_storage_iterator<std::string*,std::string> raw_it (pstr);
+
+  copy (myvector.begin(), myvector.end(), raw_it);
+
+  for (int i=0; i<3; i++)
+    std::cout << pstr[i] << ' ';
+  std::cout << '\n';
+
+  std::return_temporary_buffer(pstr);
+  return 0;
+}
+/*
+output:
+first second third 
+*/
