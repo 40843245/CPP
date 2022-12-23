a9d91ebd6049f79d2cd662d56e5d9d4aa0b0b8b5
40843245 <75050655+40843245@users.noreply.github.com>
2022-12-23 19:20:55 +0800

    Create #error ex1.cpp

---
+++ macro/special directives/#error ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#error A C++ compiler is required!
+
+int main () 
+{
+  cout<<"hello world"<<endl;
+  return 0;
+}
+
+/*
+Compiler Error:
+A C++ compiler is required!
+
+since we wrote this statement is line 4
+#error A C++ compiler is required!
+*/
